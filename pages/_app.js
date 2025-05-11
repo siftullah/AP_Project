6a77aeb427de9824fcee64d15a0b36a2b2009399
Siftullah <siftullah01@gmail.com>
2025-05-12 02:59:03 +0500

    pushed

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import "@/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import AdminLayout from "@/components/layouts/AdminLayout";
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps, router }) {
+
+  if (router.pathname.startsWith('/administration')) {
+    return (
+      <ClerkProvider>
+        <AdminLayout>
+          <Component {...pageProps} />
+        </AdminLayout>
+      </ClerkProvider>
+    );
+  }
+
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page) => page);
 
